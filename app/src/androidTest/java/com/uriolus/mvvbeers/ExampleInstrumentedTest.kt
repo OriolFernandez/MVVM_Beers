cffb2630a760f6279a721406de637dbf813445e2
Oriol Fernandez <oriol.fernandez@chartboost.com>
2021-11-24 20:37:17 +0100

    package reame

--- app/src/androidTest/java/com/uriolus/mvvbeers/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/uriolus/mvvbeers/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.uriolus.mvvmpractice", appContext.packageName)
+        assertEquals("com.uriolus.mvvbeers", appContext.packageName)
     }
 }
